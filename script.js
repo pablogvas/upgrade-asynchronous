41b1c53fc4656ad5357ed8e65134307a6f4413a4
PabloGarciaUH <pablo.garcia@bootcamp-upgrade.com>
2023-01-23 22:23:07 +0100

    Iteración #1

---
+++ script.js
@@ -0,0 +1,28 @@
+// 1.1
+
+fetch('https://api.agify.io?name=michael')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+})
+
+// 1.2
+
+  const baseUrl = "https://api.nationalize.io";
+
+  const search = (event) => {
+    const input$$ = document.querySelector("input");
+  
+    fetch(baseUrl + "?name=" + input$$.value)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+      });
+  };
+  
+  const button$ = document.querySelector("button");
+  
+  button$.addEventListener("click", search);
+
+// 1.3 
+
